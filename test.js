f8099448f9ac964d2b0af17e7a0a586a9ef2ed2d
NielsPerfectPlace <91740961+NielsPerfectPlace@users.noreply.github.com>
2021-11-23 16:21:47 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+import { UserSession } from '@esri/arcgis-rest-auth';
+import { esriId } from "@arcgis/core/identity/IdentityManager";
+
+console.log('test1')
+UserSession.beginOAuth2({
+// register an app of your own to create a unique clientId
+    clientId: "FX1W0S1vVixlMeOX",
+})
+    .then(session)
+
+var checkUser = esriId.checkSignInStatus().userId
+
+if (checkUser == "niels_PerfectPlace") { 
+    window.location.href = 'htpps://www.google.nl';
+}
+
+console.log(checkUser)
+console.log('test')
